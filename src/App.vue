8e0aac624a057f9b27482cd386c57468cdd9503d
Safaa Hammoud <hammoudsafaa@gmail.com>
2024-02-22 10:07:38 +0200

    misc(project-files): initialize project files

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
